7131332e359978ab4677d4d2ac25159391c9e569
Davidson <DAVIDSONK@dfo-mpo.ca>
2019-02-20 16:56:43 -0800

    added cloning fix

---
+++ cloning_DFO.R
@@ -0,0 +1,14 @@
+# to get github repo cloned to DFO computer
+
+# make sure repo is 'public'
+# open git bash and use: git remote add origin https://{username}:{password}@github.com/{username}/project.git
+# restart R
+# clone via https
+
+# to push
+
+# in RStudio open Tools > Shell 
+# use: git push -u origin master
+# enter credentials 
+
+#
