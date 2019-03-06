bc60fbab90380d6caee9ef6cf85db107eb630032
Davidson <DAVIDSONK@dfo-mpo.ca>
2019-03-05 16:05:57 -0800

    updated enviro plot, changes to plots

--- 2017CPUE_calibrations.R
+++ 2017CPUE_calibrations.R
@@ -54,6 +54,7 @@ flow <- data %>%
   mutate(datetime = lubridate::ymd_hm(datetime)) %>%
   separate(datetime, into=c("date", "time"), sep = " (?=[^ ]+$)") %>% 
   mutate(datetime = paste(date, time, sep=" ")) %>%
+  mutate(time = as.factor(time)) %>%
   select(date, time, datetime, USID, bay, run, current_speed_mps) %>% 
   filter(current_speed_mps != "#DIV/0!", current_speed_mps > 0) %>%   
   group_by(date, USID, run, bay, time, datetime) %>% 
@@ -95,8 +96,9 @@ flow_dis <- data %>%
         qqline(r3)
 
     
-  # FIGURE 3: Plot current speed ~ date, current speed ~ time of day, current speed ~ discharge 
+  # FIGURE 1: Plot current speed ~ date, current speed ~ time of day, current speed ~ discharge 
   # Fig 1A
+  flow$date <- as.Date(flow$date)
   date<-ggplot(flow, aes(fill=bay)) + 
     geom_point(aes(x=date, y=flow), pch=21, size=5) +
     scale_x_date(limits = as.Date(c("2017-04-03", "2017-06-14")), date_breaks = "7 day", date_labels = "%h %d") +
@@ -115,17 +117,17 @@ flow_dis <- data %>%
           axis.title.y = element_text(margin=margin(t=0,r=5,b=0,l=5), face="bold", size=30),
           axis.text.y = element_text(colour="black", size=25),
           axis.title.x = element_text(margin=margin(t=5,r=0,b=2,l=0), face="bold", size=30),
-          axis.text.x = element_text(colour="black", size=25),
+          axis.text.x = element_text(angle=45, hjust=1,colour="black", size=25),
           legend.title = element_blank(),
           legend.text = element_text(size=25),
-          legend.position = c(0.1,0.7),
+          legend.position = c(0.1,0.75),
           legend.background = element_blank(),
           legend.box.background = element_rect(colour = "black")) +
         xlab("Date") +
         ylab("")
   
   # Fig 1B    
-  flow$time <- as.POSIXct(strptime(flow$time, format="%k:%M:%S"))
+  flow$time <- as.POSIXct(flow$time, format="%H:%M:%S")
   start<-ggplot(flow, aes(fill=bay)) + 
     geom_point(aes(x=time, y=flow), pch=21, size=5) +
     scale_fill_manual(values=c("#0059d1", "#f0992d", "#81a926"), name="", 
@@ -197,9 +199,42 @@ flow_dis <- data %>%
           legend.position = "none") +
     xlab(expression(bold(paste("Discharge (m"^3, "/", s,")", sep="")))) +   
     ylab("")
-    
+  
+  # Fig 1D
+  discharge <- data %>% 
+  select(date, discharge_m3s) %>% 
+  group_by(date) %>% 
+  summarize(discharge = mean(discharge_m3s, na.rm=T))
+
+  fig1d<-ggplot(data=discharge, aes(x=date, y=discharge)) +
+    geom_line(size=1.2) +
+    scale_x_date(date_breaks = "7 day", date_labels = "%h %d") +
+    scale_y_continuous(breaks=seq(1800,18000,by=2500)) +
+    theme_bw() +
+    theme(text = element_text(colour="black", size=12),
+          plot.margin=margin(t=10,r=10,b=2,l=2),
+          panel.background = element_rect(fill = "white", colour = "black", size=2),
+          panel.grid.minor = element_line(colour = "transparent"),
+          panel.grid.major = element_line(colour = "transparent"),
+          plot.background = element_rect(fill = "transparent"),
+          axis.ticks = element_line(size=1.2),
+          axis.ticks.length = unit(0.5, "line"),
+          axis.title.y = element_text(margin=margin(t=0,r=10,b=0,l=5), face="bold", size=30),
+          axis.text.y = element_text(colour="black", size=25),
+          axis.title.x = element_text(margin=margin(t=10,r=0,b=2,l=0), face="bold", size=30),
+          axis.text.x = element_text(angle=45, hjust=1, colour="black", size=25),
+          legend.title = element_blank(),
+          legend.text = element_text(size=25),
+          legend.position = c(0.1,0.7),
+          legend.background = element_blank(),
+          legend.box.background = element_rect(colour = "black")) +
+        xlab("Date") +
+        ylab(expression(bold(paste("Discharge (m"^3, "/", s,")", sep=""))))  
+  
   grid.newpage()
-  grid.draw(rbind(ggplotGrob(date), ggplotGrob(start), ggplotGrob(dcharge), size="last"))
+  grid.draw(rbind(ggplotGrob(date), ggplotGrob(start),size="last"))
+  grid.newpage()
+  grid.draw(rbind(ggplotGrob(dcharge), ggplotGrob(fig1d),size="last"))
 
   
 ###
@@ -303,7 +338,8 @@ flow.lvl.merge <- left_join(flow, lvl.merge, by=c("date", "time", "bay", "dateti
     # Date: Current velocity increases over time, due to increasing discharge. This increase is not proportional for all Bays: Bay 6 current velocity increases much more than Bays 2 and 11 over time.
     # Discharge: As above, increasing discharge over time increases current velocity in Bay 6 much more than Bays 2 and 11
     # Time of day: Although this graph doesn't show a clear affect of time of day on current velocity, I need to combine date-time events as this change is linked to tidal cycles. Or, gather tide date. TBD.
-    
+   
+
 ######
 #
 # / END exploratory analysis
@@ -823,7 +859,7 @@ depth_m3 <- RSTVT_m3 %>%
 # Still using just the 'slice' for each trap's volume, but just with slightly altered current estimates. 
 
 ####
-# First: replace surface current entries for traps at depth with the proportional currents
+# FIRST: replace surface current entries for traps at depth with the proportional currents
 ####
 data_exp <- read.csv("mission_SO_expanded_CU_counts.csv")
 
@@ -955,7 +991,7 @@ RSTVT_m5 <- data_exp %>%
 ####
 surface_m5 <- RSTVT_m5 %>% 
   ungroup() %>%
-  select(date, run, bay, depth_ft, trap_type, fish_m3, unq_catch) %>%
+  select(date, run, bay, depth_ft, trap_type, fish_m3) %>%
   unite(DRB, date, run, bay) %>%
   filter(depth_ft == "0", fish_m3 >0)
 
@@ -1011,7 +1047,7 @@ RSTVT_m5_fac <- data_exp %>%
   mutate(fish_m3 = unq_catch/fished_vol) %>% 
   mutate(fish_m3_fac = ifelse(depth_ft != "0", fish_m3*11.8, fish_m3)) %>% 
   mutate(bay_width = 440/3) %>%                                                                                                                            # New column for Bay width (total river width = 440m/3 = Bay width)                                                                                                                                               
-  mutate(bay_depth = as.numeric(ifelse(trap_type=="Vertical", 1.01, 1.13))) %>%                                                                                                                             # Bay depth for now is considered the RST fishing depth (1.13m)
+  mutate(bay_depth = 1.13) %>%                                                                         # NOTE: as we are 'imagining' the RST is fishing at depths, we should be using the RST depth!!                                                                             # Bay depth for now is considered the RST fishing depth (1.13m)
   mutate(bay_volume_m3s = bay_width*bay_depth*current_scaled*run_time_s) %>%                                              ## NOTE CURRENT SCALED USED HERE!!!!                                        # Step 2: Volume of water in Bay during the whole run (Bay area*current velocity*run length)
   mutate(CPUE = fish_m3_fac*bay_volume_m3s)
 
@@ -1046,21 +1082,79 @@ daily_RSTVT_m5 <- RSTVT_m5_fac %>%
     mutate(propn_daily_total = catch_depth/sum(catch_depth, na.rm=T)) %>% 
     print()
 
-  # Plot
+  
+  # FIGURE 5
   depth_m3$depth_ft <- factor(depth_m3$depth_ft, levels = c("0", "6", "12"), ordered=T)
   depth_m4$depth_ft <- factor(depth_m4$depth_ft, levels = c("0", "6", "12"), ordered=T)
   depth_m5$depth_ft <- factor(depth_m5$depth_ft, levels = c("0", "6", "12"), ordered=T)
+  depth_m3$date <- as.Date(depth_m3$date)
+  depth_m4$date <- as.Date(depth_m4$date)
+  depth_m5$date <- as.Date(depth_m5$date)
 
+  # FIG 5a
   pm3<-ggplot() +
     geom_bar(data=depth_m3, aes(x=date,y=propn_daily_total, fill=depth_ft), width=1, colour="black", stat="identity") +
-    scale_fill_manual(breaks=c("0", "6", "12"),values=c("light blue", "blue", "dark blue"))
+    scale_x_date(date_breaks = "5 day", date_labels = "%h %d") +
+    scale_fill_manual(labels = c("0", "1.8", "3.7"), breaks=c("0", "6", "12"),values=c("#7fd2ea", "#0063b3", "#002b64")) +
+    theme_bw() +
+    theme(plot.margin=margin(t=10,r=20,b=0,l=45),
+          panel.background = element_rect(fill = "white", colour = "black", size=2),
+          panel.grid.minor = element_line(colour = "transparent"),
+          panel.grid.major = element_line(colour = "transparent"),
+          plot.background = element_rect(fill = "transparent"),
+          axis.ticks = element_line(size=1.2),
+          axis.ticks.length = unit(0.5, "line"),
+          axis.title.y = element_blank(),
+          axis.text.y = element_text(colour="black", size=25),
+          axis.title.x = element_blank(),
+          axis.text.x = element_blank(),
+          legend.position = "none")                                                                                   # Reduce extra white space around legend 
+  # FIG 5b
   pm4<-ggplot() +
-    geom_bar(data=depth_m4, aes(x=date,y=propn_daily_total, fill=depth_ft), width=1, colour="black", stat="identity")
+    geom_bar(data=depth_m4, aes(x=date,y=propn_daily_total, fill=depth_ft), width=1, colour="black", stat="identity") +
+    scale_x_date(date_breaks = "5 day", date_labels = "%h %d") +
+    scale_fill_manual(labels = c("0", "1.8", "3.7"), breaks=c("0", "6", "12"),values=c("#7fd2ea", "#0063b3", "#002b64")) +
+    theme_bw() +
+    theme(plot.margin=margin(t=10,r=20,b=0,l=45),
+          panel.background = element_rect(fill = "white", colour = "black", size=2),
+          panel.grid.minor = element_line(colour = "transparent"),
+          panel.grid.major = element_line(colour = "transparent"),
+          plot.background = element_rect(fill = "transparent"),
+          axis.ticks = element_line(size=1.2),
+          axis.ticks.length = unit(0.5, "line"),
+          axis.title.y = element_text(margin=margin(t=0,r=70,b=0,l=10), face="bold", size=30),
+          axis.text.y = element_text(colour="black", size=25),
+          axis.title.x = element_blank(),
+          axis.text.x = element_blank(),
+          legend.title = element_text(face="bold", size=20),
+          legend.text = element_text(size=20),
+          legend.position=c(0.8765,0.5),
+          legend.background = element_blank(),
+          legend.box.background = element_rect(fill="transparent", colour="black", size=1)) +                                                                                   # Reduce extra white space around legend 
+    guides(fill=guide_legend(title="Depth (m)")) +
+    ylab("Proportion of abundance index") 
+  # FIG 5c
   pm5<-ggplot()+
-    geom_bar(data=depth_m5, aes(x=date,y=propn_daily_total, fill=depth_ft), width=1, colour="black", stat="identity")
-
+    geom_bar(data=depth_m5, aes(x=date,y=propn_daily_total, fill=depth_ft), width=1, colour="black", stat="identity") +
+    scale_x_date(date_breaks = "5 day", date_labels = "%h %d") +
+    scale_fill_manual(labels = c("0", "1.8", "3.7"), breaks=c("0", "6", "12"),values=c("#7fd2ea", "#0063b3", "#002b64")) +
+    theme_bw() +
+    theme(plot.margin=margin(t=10,r=20,b=0,l=45),
+          panel.background = element_rect(fill = "white", colour = "black", size=2),
+          panel.grid.minor = element_line(colour = "transparent"),
+          panel.grid.major = element_line(colour = "transparent"),
+          plot.background = element_rect(fill = "transparent"),
+          axis.ticks = element_line(size=1.2),
+          axis.ticks.length = unit(0.5, "line"),
+          axis.title.y = element_blank(),
+          axis.text.y = element_text(colour="black", size=25),
+          axis.title.x = element_text(margin=margin(t=15,r=0,b=0,l=10), face="bold", size=30),
+          axis.text.x = element_text(colour="black", angle=45, hjust=1, size=25),
+          legend.position = "none") +                                                                                   # Reduce extra white space around legend 
+    xlab("Date")  
+  #fig 5 draw
   grid.newpage()
-  grid.draw(rbind(ggplotGrob(pm3), ggplotGrob(pm4), ggplotGrob(pm5), size="last"))
+  grid.draw(rbind(ggplotGrob(pm3), ggplotGrob(pm4), ggplotGrob(pm5),size="last"))
 
 
 ####
